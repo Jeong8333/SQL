4194efdb23099148a53d66b66bc40f4e5701e687
Shin Mijeong <mijungnis@naver.com>
2025-03-07 18:50:36 +0900

    Add files via upload
    250307

--- blog.sql
+++ blog.sql
@@ -26,3 +26,25 @@ WHERE student_name = '고양이';
 
 DELETE students
 WHERE student_name = '고양이';
+
+CREATE TABLE ex2(
+    mem_id VARCHAR2(50)         PRIMARY KEY     -- 기본키
+  , mem_nm VARCHAR2(50)         NOT NULL        -- 널 허용안함
+  , mem_nickname VARCHAR2(100)  UNIQUE          -- 중복 허용안함
+  , age NUMBER                                  -- 1~150
+  , gender VARCHAR2(1)                          -- F or M
+  , create_dt DATE DEFAULT SYSDATE              -- 디폴트값 설정
+  , CONSTRAINT ck_ex_age CHECK(age BETWEEN 1 AND 150)
+  , CONSTRAINT ck_ex_gender CHECK(gender IN('F','M'))
+);
+
+CREATE TABLE ex0(
+       name      VARCHAR2(50)
+     , phone_num CHAR(13) DEFAULT '010-0000-0000'
+);
+INSERT INTO ex0 (name) VALUES('홍길동');
+INSERT INTO ex1 VALUES(
+      'a001'
+    , '홍길동'
+    , '길동'
+);
