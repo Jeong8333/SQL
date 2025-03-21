a4020e136f84c9b9415ddb7033862187c687b45a
Shin Mijeong <mijungnis@naver.com>
2025-03-21 17:52:09 +0900

    Add files via upload

--- day16.sql
+++ day16.sql
@@ -40,24 +40,28 @@ AND mem_pw = 'aa';
 
 -- boards 테이블 생성
 CREATE TABLE boards (
-     board_no       NUMBER          GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 NOCACHE)  PRIMARY KEY
-    ,board_title    VARCHAR2(1000)            
-    ,mem_id         VARCHAR2(100)  CONSTRAINT boards_fk REFERENCES members(mem_id) NOT NULL                  
-    ,board_content  VARCHAR2(2000)                     
-    ,create_dt      DATE            DEFAULT SYSDATE     
-    ,update_dt      DATE            DEFAULT SYSDATE     
-    ,use_yn         VARCHAR2(1)     DEFAULT 'Y'        
+    board_no NUMBER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 NOCACHE), -- 1씩 증가 (NOCACHE 설정)
+    board_title VARCHAR2(1000),
+    mem_id VARCHAR2(100) NOT NULL,
+    board_content VARCHAR2(2000),
+    create_dt DATE DEFAULT SYSDATE,
+    update_dt DATE DEFAULT SYSDATE,
+    use_yn VARCHAR2(1) DEFAULT 'Y',
+    PRIMARY KEY (board_no), -- PRIMARY KEY 지정
+    CONSTRAINT member_fk FOREIGN KEY(mem_id) REFERENCES members(mem_id) -- FOREIGN KEY 직접 적용
 );
 
 -- boards 테이블 생성
 CREATE TABLE replys (
-     reply_no       NUMBER                  
-    ,board_no       NUMBER(10)      CONSTRAINT replys_fk REFERENCES boards(board_no)   ON DELETE CASCADE  
-    ,mem_id         VARCHAR2(100)   CONSTRAINT replys_fk_mem_id REFERENCES members(mem_id)    ON DELETE CASCADE           
-    ,reply_content  VARCHAR2(1000)                     
-    ,reply_date     DATE            DEFAULT SYSDATE     
-    ,del_yn         VARCHAR2(1)     DEFAULT 'N'
-    ,CONSTRAINT replys_PK PRIMARY KEY(reply_no, board_no)
+    reply_no NUMBER, -- 자동 증가 없음, 직접 값 입력 필요
+    board_no NUMBER(10),
+    mem_id VARCHAR2(100),
+    reply_content VARCHAR2(1000),
+    reply_date DATE DEFAULT SYSDATE,
+    del_yn VARCHAR2(1) DEFAULT 'N',
+    PRIMARY KEY (board_no, reply_no),
+    CONSTRAINT fk_mem_id FOREIGN KEY (mem_id) REFERENCES members(mem_id) ON DELETE CASCADE, -- 회원 삭제 시 관련 댓글도 삭제
+    CONSTRAINT fk_board_no FOREIGN KEY (board_no) REFERENCES boards(board_no) ON DELETE CASCADE -- 게시글 삭제 시 관련 댓글도 삭제
 );
 
 
@@ -89,9 +93,54 @@ SELECT a.board_no
 FROM boards a, members b
 WHERE a.mem_id = b.mem_id
 AND a.use_yn='Y'
-AND a.board_no = 4;
+AND a.board_no = 1;
 
 UPDATE boards
 SET board_title = 'bbbbbbb'
    ,board_content='bbbbbb'
-WHERE board_no = '5';
+WHERE board_no = '2';
+
+
+-- 댓글 저장
+INSERT INTO replys(board_no, reply_no, mem_id, reply_content)
+VALUES ('4','123456','1234','댓글 테스트');
+INSERT INTO replys(board_no, reply_no, mem_id, reply_content)
+VALUES ('12','123457','aa','댓글 테스트2');
+INSERT INTO replys(board_no, reply_no, mem_id, reply_content)
+VALUES ('4','123458','1234','댓글 테스트3');
+
+-- 댓글 1건 조회
+SELECT a.reply_no
+     , a.board_no
+     , b.mem_id
+     , b.mem_nm
+     , a.reply_content
+     ,TO_CHAR(a.reply_date, 'MM/DD HH24:MI') as reply_date
+FROM replys a, members b
+WHERE a.mem_id = b.mem_id
+AND a.del_yn = 'N'
+AND a.reply_no ='123457';
+
+-- 댓글 여러건 조회
+SELECT a.reply_no
+     , a.board_no
+     , b.mem_id
+     , b.mem_nm
+     , a.reply_content
+     ,TO_CHAR(a.reply_date, 'MM/DD HH24:MI') as reply_date
+FROM replys a, members b
+WHERE a.mem_id = b.mem_id
+AND a.del_yn = 'N'
+AND a.board_no ='4'
+ORDER BY reply_date DESC;
+
+UPDATE boards
+SET use_yn = 'Y'
+WHERE board_no = 2;
+
+SELECT *
+FROM replys;
+
+UPDATE replys
+SET del_yn = 'Y'
+WHERE reply_no = '250321113433079';
