3925ab07cec55c3a162b8bd1f161523d6802462d
Shin Mijeong <mijungnis@naver.com>
2025-04-03 19:38:30 +0900

    Add files via upload

--- ticketforget.sql
+++ ticketforget.sql
@@ -1,8 +1,8 @@
--- ticktet 계정 생성
+-- ticket 계정 생성
 ALTER SESSION SET "_ORACLE_SCRIPT"=true;
-CREATE USER ticktet IDENTIFIED BY ticktet;
-GRANT CONNECT, RESOURCE TO ticktet;
-GRANT UNLIMITED TABLESPACE TO ticktet;
+CREATE USER ticket IDENTIFIED BY ticket;
+GRANT CONNECT, RESOURCE TO ticket;
+GRANT UNLIMITED TABLESPACE TO ticket;
 
 
 -- 분류 코드
@@ -10,77 +10,66 @@ GRANT UNLIMITED TABLESPACE TO ticktet;
 // 분류항목(연극, 뮤지컬, 오페라, 음악/콘서트, 국악, 무용/발레, 전시, 기타)
 
 CREATE TABLE code_list(
-     comm_cd      VARCHAR2(4)  UNIQUE   
-    ,comm_nm      VARCHAR2(16) UNIQUE
-    ,PRIMARY KEY (comm_cd, comm_nm)
+     comm_cd      VARCHAR2(4)   PRIMARY KEY  
+    ,comm_nm      VARCHAR2(16) 
+    ,comm_parent  VARCHAR2(4)  
 );
 drop table code_list;
 
-INSERT INTO code_list
-VALUES ('TH00','연극');
-INSERT INTO code_list
-VALUES ('MU00','뮤지컬');
-INSERT INTO code_list
-VALUES ('OP00','오페라');
-INSERT INTO code_list
-VALUES ('CN00','음악');
-INSERT INTO code_list
-VALUES ('CN00','콘서트');
-INSERT INTO code_list
-VALUES ('KM00','국악');
-INSERT INTO code_list
-VALUES ('DN00','무용');
-INSERT INTO code_list
-VALUES ('DN00','발레');
-INSERT INTO code_list
-VALUES ('EX00','전시');
-INSERT INTO code_list
-VALUES ('ETC','기타');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('TH00','연극', null);         -- 연극
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('MU00','뮤지컬', null);       -- 뮤지컬
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN00','음악', null);         -- 음악
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN01','콘서트', 'CN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN02','음악', 'CN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN03','클래식', 'CN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN04','오페라', 'CN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('CN05','국악', 'CN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('DN00','무용/발레', null);    -- 무용/발레
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('DN01','무용', 'DN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('DN02','발레', 'DN00');
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('EX00','전시', null);         -- 전시회
+INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('ETC','기타', null);          -- 기타
 
---    ,charge VARCHAR2(50)            --금액
---    ,description  CLOB            --설명
+
+
 -- 문화포털 api
-
 CREATE TABLE culture(
-     comm_nm      VARCHAR2(16)          
-    ,title        VARCHAR2(1000)           
-    ,poster       VARCHAR2(1000)         
-    ,period_date  VARCHAR2(50)       
-    ,loca         VARCHAR2(1000)            
-    ,CONSTRAINT culture_fk FOREIGN KEY (comm_nm) REFERENCES code_list(comm_nm)
+     comm_cd              VARCHAR2(4)                  
+    ,title                VARCHAR2(1000)             
+    ,poster               VARCHAR2(1000)         
+    ,period_date          VARCHAR2(50)           
+    ,loc                  VARCHAR2(1000)    
+    ,culture_description  CLOB
+    ,CONSTRAINT fk_culture_comm_cd FOREIGN KEY (comm_cd) REFERENCES code_list(comm_cd)
 );
-drop table culture;
+
 
 
 -- 인터파크 티켓 크롤링
 CREATE TABLE ip_ticket(
-     comm_nm     VARCHAR2(16)         --분류항목
+     comm_cd     VARCHAR2(4)          --분류항목
     ,title       VARCHAR2(1000)       --제목
     ,poster      VARCHAR2(1000)       --이미지(썸네일 주소)
     ,period_date VARCHAR2(50)         --기간
-    ,loca        VARCHAR2(1000)       --장소
---    ,price_part VARCHAR2(50)        --가격(구역 구분 or 연령별 구분)
---    ,price NUMBER(100)              --가격(구역별, 연령별 가격)
---    ,description CLOB            --공연상세 이미지
---    ,age_limit VARCHAR2(50)         --관람연령
---    ,running_time VARCHAR2(100)     --관람시간 
-    ,CONSTRAINT fk_ip_ticket FOREIGN KEY (comm_nm) REFERENCES code_list(comm_nm)
+    ,loc         VARCHAR2(1000)       --장소
+    ,CONSTRAINT fk_ip_ticket_comm_cd FOREIGN KEY (comm_cd) REFERENCES code_list(comm_cd)
 );
-drop table ip_ticket;
+
 
 
 -- 회원 정보
 CREATE TABLE members (
-     mem_id         VARCHAR2(50)    PRIMARY KEY         -- 회원 ID
-    ,mem_pw         VARCHAR2(1000)  NOT NULL            -- 회원 비밀번호
+     mem_id         VARCHAR2(30)    PRIMARY KEY         -- 회원 ID
+    ,mem_pw         VARCHAR2(30)  NOT NULL            -- 회원 비밀번호
     ,mem_nm         VARCHAR2(30)   NOT NULL            -- 회원 이름
     ,mem_nick       VARCHAR2(100)   NOT NULL            -- 회원 닉네임
-    ,mem_em         VARCHAR2(1000)                      -- 회원 메일 주소
+    ,mem_email      VARCHAR2(1000)                      -- 회원 메일 주소
     ,profile_img    VARCHAR2(1000)                      -- 프로필 이미지 URL 또는 경로
     ,use_yn         VARCHAR2(1)     DEFAULT 'Y'         -- 사용 여부(Y 또는 N)
-    ,update_dt      DATE            DEFAULT SYSDATE     -- 정보 수정일
-    ,create_dt      DATE            DEFAULT SYSDATE     -- 정보 생성일
+    ,create_date    DATE            DEFAULT SYSDATE     -- 정보 생성일
+    ,update_date    DATE            DEFAULT SYSDATE     -- 정보 수정일
 );
+
 
 
 -- 기록
@@ -102,3 +91,52 @@ CREATE TABLE post(
      ,CONSTRAINT fk_post_title FOREIGN KEY (title) REFERENCES            (title)
      ,CONSTRAINT fk_post_loca FOREIGN KEY (loca) REFERENCES            (loca)
 );
+
+
+
+-- ===조회==============================================================
+
+SELECT *
+FROM culture;
+
+SELECT *
+FROM ip_ticket;
+
+
+SELECT b.comm_cd
+     , b.comm_nm
+    , COUNT(b.comm_cd)
+FROM culture a, code_list b
+WHERE a.comm_cd = b.comm_cd
+GROUP BY b.comm_cd, b.comm_nm;
+
+
+SELECT b.comm_cd
+     , b.comm_nm
+    , COUNT(b.comm_cd)
+FROM ip_ticket a, code_list b
+WHERE a.comm_cd = b.comm_cd
+GROUP BY b.comm_cd, b.comm_nm;
+
+WITH A as(  SELECT comm_cd, title, poster, period_date,loc, culture_description
+                  ,TO_DATE(SUBSTR(period_date,1,8)) as start_date    -- 공연 시작일
+                  ,TO_DATE(SUBSTR(period_date,12,8)) as end_date     -- 공연 종료일
+            FROM culture
+            WHERE poster IS NOT NULL
+)
+
+
+
+SELECT a. comm_cd, b.comm_nm, a.title, a.poster, a.period_date, a.loc
+FROM ip_ticket a, code_list b
+WHERE a.comm_cd = b.comm_cd
+ORDER BY a.comm_cd, a.title;
+
+
+-- culture테이블과 ip_ticket 테이블을 comm_cd로 조인
+
+
+
+
+
+
