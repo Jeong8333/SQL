5df1ea83e2f86c1e5f7072cd24305cce2e3f30ad
Shin Mijeong <mijungnis@naver.com>
2025-04-08 19:40:17 +0900

    Add files via upload

--- ticketforget.sql
+++ ticketforget.sql
@@ -14,7 +14,7 @@ CREATE TABLE code_list(
     ,comm_nm      VARCHAR2(16) 
     ,comm_parent  VARCHAR2(4)  
 );
-drop table code_list;
+
 
 
 
@@ -29,6 +29,8 @@ CREATE TABLE culture(
     ,CONSTRAINT fk_culture_comm_cd FOREIGN KEY (comm_cd) REFERENCES code_list(comm_cd)
 );
 
+ALTER TABLE tb_culture
+RENAME COLUMN loc TO addr;
 
 
 -- 인터파크 티켓 크롤링
@@ -42,41 +44,23 @@ CREATE TABLE ip_ticket(
 );
 
 
-drop table members;
 
 -- 회원 정보
 CREATE TABLE members (
      mem_id         VARCHAR2(1000)    PRIMARY KEY         -- 회원 ID
     ,mem_pw         VARCHAR2(1000)    NOT NULL            -- 회원 비밀번호
     ,mem_nm         VARCHAR2(1000)    NOT NULL            -- 회원 이름
-    ,mem_nick       VARCHAR2(1000)   NOT NULL            -- 회원 닉네임
-    ,mem_addr       VARCHAR2(1000)  NOT NULL            -- 회원 메일 주소
-    ,profile_img    VARCHAR2(1000)                      -- 프로필 이미지 URL 또는 경로
-    ,create_date    DATE            DEFAULT SYSDATE     -- 정보 생성일
-    ,update_date    DATE            DEFAULT SYSDATE     -- 정보 수정일
-    ,use_yn         VARCHAR2(1)     DEFAULT 'Y'         -- 사용 여부(Y 또는 N)
-);
+    ,mem_nick       VARCHAR2(1000)    NOT NULL            -- 회원 닉네임
+    ,mem_addr       VARCHAR2(1000)    NOT NULL            -- 회원 메일 주소
+    ,profile_img    VARCHAR2(1000)                        -- 프로필 이미지 URL 또는 경로
+    ,create_date    DATE              DEFAULT SYSDATE     -- 정보 생성일
+    ,update_date    DATE              DEFAULT SYSDATE     -- 정보 수정일
+    ,use_yn         VARCHAR2(1)       DEFAULT 'Y'         -- 사용 여부(Y 또는 N)
+); 
 
 
--- 기록
-CREATE TABLE post(
-      mem_id        VARCHAR2(50)            -- 회원 id
-     ,comm_cd       VARCHAR2(4)             -- 분류코드
-     ,comm_nm       VARCHAR2(16)            -- 분류 항목
-     ,title         VARCHAR2(1000)          -- 공연명
-     ,loca          VARCHAR2(1000)          -- 장소
-     ,viewing_date  DATE                    -- 관람일
-     ,post_date     DATE DEFAULT SYSDATE    -- 작성일
-     ,friend        VARCHAR2(30)            -- 동행인
-     ,rating        NUMBER                  -- 별점
-     ,review        CLOB                    -- 관람평
-     ,image_url     VARCHAR2(1000)          -- 첨부 사진 경로
-     ,CONSTRAINT fk_post_id FOREIGN KEY (mem_id) REFERENCES members(mem_id)
-     ,CONSTRAINT fk_post_cd FOREIGN KEY (comm_cd) REFERENCES code_list(comm_cd)
-     ,CONSTRAINT fk_post_cdnm FOREIGN KEY (comm_nm) REFERENCES code_list(comm_nm)
-     ,CONSTRAINT fk_post_title FOREIGN KEY (title) REFERENCES            (title)
-     ,CONSTRAINT fk_post_loca FOREIGN KEY (loca) REFERENCES            (loca)
-);
+
+
 
 -- ===INSERT==========================================
 INSERT INTO code_list (comm_cd,comm_nm,comm_parent) VALUES ('TH00','연극', null);         -- 연극
@@ -101,6 +85,94 @@ INSERT INTO members (
          , mem_addr)
 VALUES ('test1', 1234, '테스트', '테스트계정', 'aa');
 
+SELECT mem_id
+     , mem_pw
+     , mem_nm
+     , mem_nick
+     , mem_addr
+     , profile_img
+FROM members
+WHERE use_yn = 'Y'
+AND mem_id = 'test1';
+
+SELECT comm_cd      
+      ,comm_nm     
+      ,comm_parent 
+FROM code_list;
+
+-- 중복 데이터 제거
+DELETE FROM culture
+WHERE title IN (
+                SELECT c.title
+                FROM culture c
+                JOIN ip_ticket i ON c.title = i.title
+);
+
+-- 기록
+CREATE TABLE reviews(
+      review_no     NUMBER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 NOCACHE)
+     ,mem_id        VARCHAR2(1000)          -- 회원 id
+     ,ticket_no     NUMBER                  -- 인터파크 티켓 테이블 id
+     ,culture_no    NUMBER                  -- 문화 테이블 id
+     ,comm_cd       VARCHAR2(4)             -- 분류코드
+     ,comm_nm       VARCHAR2(16)            -- 분류 항목
+     ,title         VARCHAR2(1000)          -- 공연명
+     ,poster        VARCHAR2(1000)          -- 이미지(썸네일 주소)
+     ,loc           VARCHAR2(1000)          -- 장소
+     ,viewing_date  DATE                    -- 관람일
+     ,review_date   DATE DEFAULT SYSDATE    -- 작성일
+     ,update_date   DATE DEFAULT SYSDATE    -- 수정일
+     ,friend        VARCHAR2(100)           -- 동행인
+     ,rating        NUMBER                  -- 별점
+     ,review        CLOB                    -- 관람평
+     ,photo         CLOB                    -- 첨부 사진 경로
+     ,del_yn         VARCHAR2(1)  DEFAULT 'N'   -- 삭제 여부(Y 또는 N)
+     ,PRIMARY KEY (review_no)
+     ,CONSTRAINT fk_review_mem_id       FOREIGN KEY (mem_id)        REFERENCES members(mem_id)   
+     ,CONSTRAINT fk_review_ticket_no    FOREIGN KEY (ticket_no)     REFERENCES tb_ticket(ticket_no)   
+     ,CONSTRAINT fk_review_culture_no   FOREIGN KEY (culture_no)    REFERENCES tb_culture(culture_no)   
+);
+
+-- =================================================
+SELECT *
+FROM(
+        SELECT c.comm_cd,
+               c.comm_nm,
+               a.title,
+               a.poster,
+               a.period_date,
+               a.addr,
+               a.culture_description
+        FROM (SELECT ticket_no, comm_cd, title, poster, period_date, addr,  NULL AS culture_description 
+              FROM tb_ticket
+              UNION ALL
+              SELECT culture_no, comm_cd, title, poster, period_date, addr, culture_description
+              FROM tb_culture
+              ) a
+        JOIN code_list c ON c.comm_cd = a.comm_cd
+        WHERE c.comm_nm ='뮤지컬')
+        
+WHERE title LIKE '%명성%';
+
+SELECT ticket_no, title
+FROM tb_ticket
+WHERE title LIKE '%명성%';
+
+INSERT INTO reviews( mem_id , ticket_no, comm_cd, comm_nm, title, poster, loc
+                    , viewing_date, friend, rating, review, photo)
+VALUES('testtest', 1660, 'MU00', '뮤지컬', '뮤지컬 〈명성황후〉 30주년 기념 공연 - 대전', 'https://ticketimage.interpark.com/Play/image/large/25/25004164_p.gif', '대전예술의전당 아트홀'
+      , '25/04/06', '홍길동', '4.2', '리뷰 내용', '이미지 주소');
+
+
+
+
+
+
+
+
+
+
+
 -- ===조회==============================================================
 
 -- code 대분류
@@ -114,13 +186,13 @@ WHERE comm_parent = 'DN00';
 
 -- 문화포털 api
 SELECT *
-FROM culture;
+FROM tb_culture;
 
 -- 인터파크 티켓 크롤링
 SELECT *
 FROM ip_ticket;
 SELECT COUNT(title)
-FROM ip_ticket;
+FROM tb_ticket;
 
 -- 회원 조회
 SELECT mem_id
@@ -164,7 +236,7 @@ SELECT b.comm_nm, a.title, a.poster, a.start_date, a.end_date, a.culture_descrip
 FROM a, code_list b
 WHERE a.comm_cd = b.comm_cd
 AND a.comm_cd = 'TH00'
-ORDER BY a.end_date;
+ORDER BY a.end_date desc;
 
 
 SELECT b.comm_nm, a.title, a.poster, a.period_date, a.loc
